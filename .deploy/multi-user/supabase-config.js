8c920135dacb072d002151a2683c13ee32f5a0f1
Mostafa <yousrey.mostafa@gmail.com>
2026-08-25 12:17:39 +0300

    Add multi-user Supabase config template

---
+++ .deploy/multi-user/supabase-config.js
@@ -0,0 +1,11 @@
+/*
+  GROHE SpecBuilder multi-user configuration.
+  Create a Supabase project, then paste the Project URL and anon/public key below.
+  The anon key is designed to be public in browser apps; privacy is enforced by RLS policies.
+*/
+window.GROHE_SUPABASE_CONFIG = Object.freeze({
+  url: '',
+  anonKey: '',
+  allowSignUp: true,
+  appName: 'GROHE SpecBuilder'
+});
