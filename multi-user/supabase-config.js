01eda9faf1907f1bfa19c9b26fd6be50d3ed80f4
Mostafa <yousrey.mostafa@gmail.com>
2026-08-25 12:24:21 +0300

    Add multi-user Supabase configuration

---
+++ multi-user/supabase-config.js
@@ -0,0 +1,7 @@
+/* GROHE SpecBuilder multi-user configuration. */
+window.GROHE_SUPABASE_CONFIG = Object.freeze({
+  url: '',
+  anonKey: '',
+  allowSignUp: true,
+  appName: 'GROHE SpecBuilder'
+});
